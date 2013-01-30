b019fd7e0b03f4f07b45b2eef5fc0978f2c5b4c4
Swami Atma <swami@ohlalaweb.com>
2013-01-30 10:37:40 +0100

    Implemented autocomplete of parent menu name in menu form.

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -5,7 +5,7 @@ end
 
 class Menu < ActiveRecord::Base
   
-  attr_accessible :name, :path, :position, :parent_id
+  attr_accessible :name, :path, :position, :parent_id, :parent_menu_name
   
   validates_presence_of :name, :path
   
@@ -15,6 +15,7 @@ class Menu < ActiveRecord::Base
     name ? "#{id}-#{name.to_url}" : id
   end
   
+  scope :alphabetical, order('name')
   scope :ordered, order('position, id')
   # tree scopes
   scope :orphans, where('parent_id IS ?', nil)
@@ -42,6 +43,19 @@ class Menu < ActiveRecord::Base
   
   def siblings
     Menu.with_parent_id(parent_id)
+  end
+  
+  ### autocomplete related instance methods
+  def parent_menu_name
+      parent.try(:name) unless self.orphan?
+    end
+
+  def parent_menu_name=(name)
+    if name.present? && Menu.find_by_name(name)
+      self.parent_id = Menu.find_by_name(name).id
+    else
+      self.parent_id = nil
+    end
   end
   
 end
